d6be331ac8f444891e24b796624a628e26d750a7
SafwanChowdhury <safwanrija@yahoo.co.uk>
2023-02-05 15:49:30 +0000

    added 3d obj files

--- 3d-chess/main.js
+++ 3d-chess/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import {OBJLoader} from "three/addons/loaders/OBJLoader.js";
 
 const scene = new THREE.Scene();
 
@@ -28,37 +29,15 @@ scene.add(ambientLight)
 
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
+scene.add(lightHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-function addStar(){
-    const geometry = new THREE.SphereGeometry(0.25, 24,24);
-    const material = new THREE.MeshStandardMaterial({color: 0xffffff})
-    const star = new THREE.Mesh(geometry, material)
-
-    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-
-    star.position.set(x,y,z);
-
-    scene.add(star)
-}
-
-Array(200).fill().forEach(addStar)
 
 
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
-
-const jeff = new THREE.Mesh(
-    new THREE.BoxGeometry(3,3,3),
-    new THREE.MeshBasicMaterial( { map: jeffTexture })
-);
-
-scene.add(jeff)
 
 const moonTexture = new THREE.TextureLoader().load('moon.jpg');
 const normalTexture = new THREE.TextureLoader().load('normal.jpg');
@@ -77,22 +56,10 @@ scene.add(moon)
 moon.position.z = 30;
 moon.position.setX(-10);
 
-function moveCamera(){
-    const t = document.body.getBoundingClientRect().top;
-    moon.rotation.x += 0.05;
-    moon.rotation.y += 0.075;
-    moon.rotation.z += 0.05;
+const loader = new OBJLoader();
 
-    jeff.rotation.y += 0.01;
-    jeff.rotation.z += 0.01;
+loader.load('')
 
-    camera.position.z = t * -0.01;
-    camera.position.x = t * -0.0002;
-    camera.rotation.y = t * -0.0002;
-}
-
-document.body.onscroll = moveCamera;
-moveCamera();
 function animate(){
     requestAnimationFrame(animate);
 
