393eecbaec853eb6ae67e33e63cd2f6ec9f86c04
SafwanChowdhury <safwanrija@yahoo.co.uk>
2023-03-14 03:03:03 +0000

    added grid finder python code, helper for debugging

---
+++ 3d-chess/grid finder.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+
+class GridWindow(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.grid_cells = []
+        self.selected_cell = None
+        self.create_widgets()
+
+    def create_widgets(self):
+        for row in range(8):
+            grid_row = []
+            for col in range(8):
+                cell = tk.Label(self.master, text='', relief=tk.RIDGE, width=5, height=2)
+                cell.grid(row=row, column=col, padx=1, pady=1)
+                cell.bind("<Button-1>", self.cell_clicked)
+                grid_row.append(cell)
+            self.grid_cells.append(grid_row)
+
+    def cell_clicked(self, event):
+        clicked_cell = event.widget
+        if self.selected_cell:
+            self.selected_cell.config(bg='white', text='')
+        self.selected_cell = clicked_cell
+        clicked_cell.config(bg='red', text='x')
+        self.update_grid()
+
+    def update_grid(self):
+        if self.selected_cell:
+            selected_index = self.get_selected_indexes()[0] * 8 + self.get_selected_indexes()[1]
+            for row in range(8):
+                for col in range(8):
+                    current_index = row * 8 + col
+                    distance = current_index - selected_index
+                    if current_index == selected_index:
+                        self.grid_cells[row][col].config(text='x')
+                    else:
+                        self.grid_cells[row][col].config(text=str(distance))
+
+
+    def get_selected_indexes(self):
+        for row in range(8):
+            for col in range(8):
+                if self.grid_cells[row][col] == self.selected_cell:
+                    return row, col
+
+root = tk.Tk()
+app = GridWindow(master=root)
+app.mainloop()
