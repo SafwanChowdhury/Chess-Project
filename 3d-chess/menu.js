525546482962494ccbdd88054a5cb482dc384577
SafwanChowdhury <safwanrija@yahoo.co.uk>
2023-04-06 06:16:34 +0100

    added issue reporting ability. connected to github

--- 3d-chess/menu.js
+++ 3d-chess/menu.js
@@ -7,3 +7,8 @@ burger.addEventListener("click", () => {
     document.getElementById("menu-glow").style.opacity = menu.classList[1] == 'active' ? 1 : 0;
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    menu.classList.toggle("active");
+    document.getElementById("menu-glow").style.opacity = 1;
+});
