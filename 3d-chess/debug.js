cf17b7e264d02afe71688473778e834a14d414e3
SafwanChowdhury <safwanrija@yahoo.co.uk>
2023-04-28 13:48:15 +0100

    reworked code structure for 3D scene loading, added wizard pieces, optimised code to improve load times

--- 3d-chess/debug.js
+++ 3d-chess/debug.js
@@ -1,169 +1,11 @@
 import "./CSS/game.css";
 import "./CSS/loading.css";
 import "./CSS/popup.css";
-
 import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 import {game} from "./script";
 import {CSS3DObject} from "three/addons/renderers/CSS3DRenderer.js";
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector("#bg"),
-});
-
-function initScene() {
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize( window.innerWidth, window.innerHeight);
-	camera.position.set(0 , 0, 20);
-
-	const pointLight = new THREE.PointLight(0xffffff, 0.5);
-	pointLight.position.set(0, 15, 0);
-	scene.add(pointLight);
-
-	const pointLight2 = new THREE.PointLight(0xffffff, 0.2);
-	pointLight2.position.set(0, 15, 20);
-	scene.add(pointLight2);
-
-	const pointLight3 = new THREE.PointLight(0xffffff, 0.2);
-	pointLight3.position.set(0, 15, -20);
-	scene.add(pointLight3);
-
-	const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-	scene.add(ambientLight);
-
-
-	const lightHelper = new THREE.PointLightHelper(pointLight2);
-	const gridHelper = new THREE.GridHelper(16.9, 8, 0x000000, 0xffffff);
-	scene.add(lightHelper, gridHelper);
-
-	const loader2 = new THREE.CubeTextureLoader();
-	scene.background = loader2.load([
-		"skybox/right.png",
-		"skybox/left.png",
-		"skybox/top.png",
-		"skybox/bottom.png",
-		"skybox/front.png",
-		"skybox/back.png",
-	]);
-
-	const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-	const normalTexture = new THREE.TextureLoader().load("normal.jpg");
-
-	const moon = new THREE.Mesh(
-		new THREE.SphereGeometry(3, 32, 32),
-		new THREE.MeshStandardMaterial({
-			map: moonTexture,
-			normalMap: normalTexture
-		})
-	);
-
-	scene.add(moon);
-	moon.position.set(0, 25, 0);
-
-	const loader = new GLTFLoader();
-	loader.load(
-		"models/ChessBoard.glb",
-		function (gltf) {
-			scene.add(gltf.scene);
-		}
-	);
-}
-function createBoardSquares() {
-	let boardSquares = [];
-	let squareSize = 2.1; // change this to adjust the size of the squares
-
-	for (let i = 0; i < 8; i++) {
-		for (let j = 0; j < 8; j++) {
-			let squareColor = 0x00FF00;
-			let squareGeometry = new THREE.BoxGeometry(squareSize, 0.05, squareSize);
-			let squareMaterial = new THREE.MeshLambertMaterial({ color: squareColor, transparent: true, opacity: 0 });
-			let squareMesh = new THREE.Mesh(squareGeometry, squareMaterial);
-			squareMesh.position.set((j - 3.5) * squareSize, 0 , (i - 3.5) * squareSize);
-			boardSquares.push(squareMesh);
-		}
-	}
-
-	return boardSquares;
-}
-
-const boardSquares = createBoardSquares();
-for (let i = 0; i < boardSquares.length; i++) {
-	boardSquares[i].userData.index = i;
-	scene.add(boardSquares[i]);
-}
-
-let pieces = [];
-let whitePieces = [];
-let blackPieces = [];
-let takenWhite = [];
-let takenBlack = [];
-
-const objArray = [
-	"models/wRookR.glb",
-	"models/wKnightR.glb",
-	"models/wBishopR.glb",
-	"models/wKing.glb",
-	"models/wQueen.glb",
-	"models/wBishopL.glb",
-	"models/wKnightL.glb",
-	"models/wRookL.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bRookR.glb",
-	"models/bKnightR.glb",
-	"models/bBishopR.glb",
-	"models/bKing.glb",
-	"models/bQueen.glb",
-	"models/bBishopL.glb",
-	"models/bKnightL.glb",
-	"models/bRookL.glb"
-];
-
-const coordsMap = [-7.36, -5.36, -3.16, -1.06, 1.06, 3.16, 5.16, 7.36];
-const takenMap = [11.56, 13.66];
-
-const manager = new THREE.LoadingManager();
-function loadObject(i, obj, x1, z1) {
-	const loader = new GLTFLoader(manager);
-	loader.load(obj, function(gltf){
-		pieces[i] = gltf.scene;
-		pieces[i].position.x = coordsMap[x1];
-		pieces[i].position.z = coordsMap[z1];
-		scene.add(pieces[i]);
-	});
-}
-
-let lenObj = objArray.length;
-
-const initArray = [
-	{x: 0, y: 0} , {x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}, {x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 7, y: 0},
-	{x: 0, y: 1} , {x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 6, y: 1}, {x: 7, y: 1},
-	{x: 0, y: 6} , {x: 1, y: 6}, {x: 2, y: 6}, {x: 3, y: 6}, {x: 4, y: 6}, {x: 5, y: 6}, {x: 6, y: 6}, {x: 7, y: 6},
-	{x: 0, y: 7} , {x: 1, y: 7}, {x: 2, y: 7}, {x: 3, y: 7}, {x: 4, y: 7}, {x: 5, y: 7}, {x: 6, y: 7}, {x: 7, y: 7}
-];
-
-function objectLoading(){
-	for (let i = 0; i < lenObj; i++) {
-		loadObject(i, objArray[i], initArray[i].x, initArray[i].y);
-	}
-}
+import {initScene,pieces,blackPieces,whitePieces,camera,renderer,scene,boardSquares,coordsMap,takenMap,takenWhite, initArray,takenBlack,loadQueen,objectLoading,manager} from "./scene.js";
 
 function addPieceData(){
 	for (let i = 0; i < pieces.length; i++){
@@ -189,11 +31,6 @@ function addPieceData(){
 			blackPieces.push(pieces[i]);
 		}
 	}
-}
-
-function loadQueen(i, obj, x1, z1){
-	scene.remove(pieces[i]);
-	loadObject(i, obj, x1, z1);
 }
 
 function addPromotionData(){
@@ -334,13 +171,6 @@ manager.onLoad = function () {
 	}
 };
 
-function resize_window(camera, renderer){
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio);
-}
-
 const turnOverlay = document.getElementById("turn-overlay");
 const turnText = document.getElementById("turn-text");
 const popup = document.getElementById("end-screen");
@@ -365,7 +195,5 @@ box.hidden = true;
 const turnOverlayObject = new CSS3DObject(turnOverlay);
 scene.add(turnOverlayObject);
 
-
-window.addEventListener("resize",() => resize_window(camera,renderer));
 
 objectLoading();
--- 3d-chess/main.js
+++ 3d-chess/main.js
@@ -4,163 +4,9 @@ import "./CSS/popup.css";
 import socket from "./socket.js";
 import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 import {game} from "./script";
-import { CSS2DRenderer, CSS2DObject } from "three/addons/renderers/CSS2DRenderer.js";
-import { CSS3DRenderer, CSS3DObject } from "three/addons/renderers/CSS3DRenderer.js";
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector("#bg"),
-});
-
-function initScene() {
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize( window.innerWidth, window.innerHeight);
-	camera.position.set(0 , 0, 20);
-
-	const pointLight = new THREE.PointLight(0xffffff, 0.5);
-	pointLight.position.set(0, 15, 0);
-	scene.add(pointLight);
-
-	const pointLight2 = new THREE.PointLight(0xffffff, 0.2);
-	pointLight2.position.set(0, 15, 20);
-	scene.add(pointLight2);
-
-	const pointLight3 = new THREE.PointLight(0xffffff, 0.2);
-	pointLight3.position.set(0, 15, -20);
-	scene.add(pointLight3);
-
-	const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-	scene.add(ambientLight);
-
-	const loader2 = new THREE.CubeTextureLoader();
-	scene.background = loader2.load([
-		"skybox/right.png",
-		"skybox/left.png",
-		"skybox/top.png",
-		"skybox/bottom.png",
-		"skybox/front.png",
-		"skybox/back.png",
-	]);
-
-	const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-	const normalTexture = new THREE.TextureLoader().load("normal.jpg");
-
-	const moon = new THREE.Mesh(
-		new THREE.SphereGeometry(3, 32, 32),
-		new THREE.MeshStandardMaterial({
-			map: moonTexture,
-			normalMap: normalTexture
-		})
-	);
-
-	scene.add(moon);
-	moon.position.set(0, 25, 0);
-
-	const loader = new GLTFLoader();
-	loader.load(
-		"models/ChessBoard.glb",
-		function (gltf) {
-			scene.add(gltf.scene);
-		}
-	);
-}
-function createBoardSquares() {
-	let boardSquares = [];
-	let squareSize = 2.1; // change this to adjust the size of the squares
-
-	for (let i = 0; i < 8; i++) {
-		for (let j = 0; j < 8; j++) {
-			let squareColor = 0x00FF00;
-			let squareGeometry = new THREE.BoxGeometry(squareSize, 0.05, squareSize);
-			let squareMaterial = new THREE.MeshLambertMaterial({ color: squareColor, transparent: true, opacity: 0 });
-			let squareMesh = new THREE.Mesh(squareGeometry, squareMaterial);
-			squareMesh.position.set((j - 3.5) * squareSize, 0 , (i - 3.5) * squareSize);
-			boardSquares.push(squareMesh);
-		}
-	}
-
-	return boardSquares;
-}
-
-const boardSquares = createBoardSquares();
-for (let i = 0; i < boardSquares.length; i++) {
-	boardSquares[i].userData.index = i;
-	scene.add(boardSquares[i]);
-}
-
-let pieces = [];
-let whitePieces = [];
-let blackPieces = [];
-let takenWhite = [];
-let takenBlack = [];
-
-const objArray = [
-	"models/wRookR.glb",
-	"models/wKnightR.glb",
-	"models/wBishopR.glb",
-	"models/wKing.glb",
-	"models/wQueen.glb",
-	"models/wBishopL.glb",
-	"models/wKnightL.glb",
-	"models/wRookL.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/wPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bPawn.glb",
-	"models/bRookR.glb",
-	"models/bKnightR.glb",
-	"models/bBishopR.glb",
-	"models/bKing.glb",
-	"models/bQueen.glb",
-	"models/bBishopL.glb",
-	"models/bKnightL.glb",
-	"models/bRookL.glb"
-];
-
-const coordsMap = [-7.36, -5.36, -3.16, -1.06, 1.06, 3.16, 5.16, 7.36];
-const takenMap = [11.56, 13.66];
-
-const manager = new THREE.LoadingManager();
-function loadObject(i, obj, x1, z1) {
-	const loader = new GLTFLoader(manager);
-	loader.load(obj, function(gltf){
-		pieces[i] = gltf.scene;
-		pieces[i].position.x = coordsMap[x1];
-		pieces[i].position.z = coordsMap[z1];
-		scene.add(pieces[i]);
-	});
-}
-
-let lenObj = objArray.length;
-
-const initArray = [
-	{x: 0, y: 0} , {x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}, {x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 7, y: 0},
-	{x: 0, y: 1} , {x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 6, y: 1}, {x: 7, y: 1},
-	{x: 0, y: 6} , {x: 1, y: 6}, {x: 2, y: 6}, {x: 3, y: 6}, {x: 4, y: 6}, {x: 5, y: 6}, {x: 6, y: 6}, {x: 7, y: 6},
-	{x: 0, y: 7} , {x: 1, y: 7}, {x: 2, y: 7}, {x: 3, y: 7}, {x: 4, y: 7}, {x: 5, y: 7}, {x: 6, y: 7}, {x: 7, y: 7}
-];
-
-function objectLoading(){
-	for (let i = 0; i < lenObj; i++) {
-		loadObject(i, objArray[i], initArray[i].x, initArray[i].y);
-	}
-}
-
+import { CSS3DObject } from "three/addons/renderers/CSS3DRenderer.js";
+import {initScene,pieces,blackPieces,whitePieces,camera,renderer,scene,boardSquares,coordsMap,takenMap,takenWhite, initArray,takenBlack,loadQueen,objectLoading,manager} from "./scene.js";
 function addPieceData(){
 	for (let i = 0; i < pieces.length; i++){
 		pieces[i].userData.pieceId = i;
@@ -185,11 +31,6 @@ function addPieceData(){
 			blackPieces.push(pieces[i]);
 		}
 	}
-}
-
-function loadQueen(i, obj, x1, z1){
-	scene.remove(pieces[i]);
-	loadObject(i, obj, x1, z1);
 }
 
 function addPromotionData(){
@@ -236,14 +77,67 @@ function onDocumentMouseDown(event) {
 	}
 }
 
-
 let modified = [];
 let modifiedData = [];
 let promotion = false;
 let received = false;
+let connected = false;
+let cameraMoved = false;
+
+
 function animate() {
 	requestAnimationFrame(animate);
 	camControls.enabled = gameLogic.selected === null;
+	if (connected && cameraMoved === false){
+		if (clientID[0]){
+			if (camera.position.x > 0.20) {
+				camera.position.x = camera.position.x - 0.1
+			}
+			if (camera.position.y > 16.3) {
+				camera.position.y = camera.position.y - (1 / 39)
+			}
+			if (camera.position.z > -11.5) {
+				camera.position.z = camera.position.z - (17 / 120)
+			}
+			if (camera.rotation.x > -2.1845696263287486) {
+				camera.rotation.x = camera.rotation.x - (0.58918/78)
+			}
+			if (camera.rotation.y > -0.009568585566868008) {
+				camera.rotation.y = camera.rotation.y - (0.42157/78)
+			}
+			if (camera.rotation.z < 3.128012569910931) {
+				camera.rotation.z = camera.rotation.z + (1.49589/78)
+			}
+			else{
+				cameraMoved = true;
+				camControls.enabled = true;
+			}
+		}
+		else {
+			if (camera.position.x > -0.10) {
+				camera.position.x = camera.position.x - 0.1
+			}
+			if (camera.position.y > 16.3) {
+				camera.position.y = camera.position.y - (1 / 39)
+			}
+			if (camera.position.z < 11.5) {
+				camera.position.z = camera.position.z + (17 / 120)
+			}
+			if (camera.rotation.x < -0.9289476942162901) {
+				camera.rotation.x = camera.rotation.x + (0.66644/78)
+			}
+			if (camera.rotation.y > 0.009568585566868008) {
+				camera.rotation.y = camera.rotation.y - (0.41201282561550545/78)
+			}
+			if (camera.rotation.z > -0.007967086578719378) {
+				camera.rotation.z = camera.rotation.z - (1.62416/78)
+			}
+			else{
+				cameraMoved = true;
+				camControls.enabled = true;
+			}
+		}
+	}
 	modified = gameLogic.modified;
 	sessionStorage.setItem("movesLog", JSON.stringify(gameLogic.movesLog));
 	if (modified.length > 0){
@@ -287,6 +181,8 @@ function animate() {
 	renderer.render(scene,camera);
 }
 
+
+
 let onStart = false;
 
 manager.onStart = function (url, itemsLoaded, itemsTotal) {
@@ -314,12 +210,6 @@ manager.onLoad = function () {
 		initScene();
 		addPieceData();
 		gameLogic.initKing();
-		camera.position.x = 8;
-		camera.position.y = 18.3;
-		camera.position.z = -0.45;
-		camera.rotation.x = -1.59;
-		camera.rotation.y = 0.41;
-		camera.rotation.z = 1.63;
 		loaded = 1;
 		animate();
 	}
@@ -328,14 +218,6 @@ manager.onLoad = function () {
 	}
 };
 
-function resize_window(camera, renderer){
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio);
-}
-
-window.addEventListener("resize",() => resize_window(camera,renderer));
 let loaded = 0;
 let clientID = [];
 let roomId = null;
@@ -359,6 +241,12 @@ function initialiseGame(){
 			console.log(clientID[0]);
 			updateTurnOverlay();
 			turnOverlay.hidden = false;
+			break;
+		case "start":
+			camera.position.set(8, 18.3, -0.1369);
+			camera.rotation.set(-1.578, 0.41, 1.589);
+			camControls.enabled = false;
+			connected = true;
 			break;
 		case "action":
 			gameLogic.unitTest(message.data[0], message.data[1]);
@@ -415,6 +303,13 @@ function takeScreenshot() {
 	const dataURL = renderer.domElement.toDataURL("image/png");
 	sessionStorage.setItem("screenshot", dataURL);
 }
+
+document.addEventListener('keydown', (event) => {
+	if (event.code === 'Space') {
+		console.log('Camera position:', camera.position);
+		console.log('Camera rotation:', camera.rotation);
+	}
+});
 
 objectLoading();
 
